175c6037a0f0c82cc717bb59ddae01ce3ae9742a
DoanTrungg <150586112+DoanTrungg@users.noreply.github.com>
2024-10-10 20:40:03 +0700

    * change swipe machine

--- Assets/Dot.cs
+++ Assets/Dot.cs
@@ -6,64 +6,8 @@ using UnityEngine;
 
 public class Dot : MonoBehaviour
 {
-    private Vector2 _firstTouchPosition;
-    private Vector2 _finalTouchPosition;
-    private float _swipeAngle;
-    private float _swipeDistance;
     private BackgroundTile _backgroundTile;
-    private Board _boad;
     
-
-    public float SwipeAngle { get => _swipeAngle; set => _swipeAngle = value; }
     public BackgroundTile BackgroundTile { get => _backgroundTile; set => _backgroundTile = value; }
-    public float SwipeDistance { get => _swipeDistance; set => _swipeDistance = value; }
-
-    private void Awake()
-    {
-        _boad = Board.Instance.GetComponent<Board>();
-    }
-
-    private void OnMouseDown()
-    {
-        _firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    }
-    private void OnMouseUp()
-    {
-        _finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        CalculateAngle();
-        MovePieces((int)_swipeAngle, _swipeDistance);
-        
-    }
-    private void CalculateAngle()
-    {
-        _swipeAngle = Mathf.Atan2(_finalTouchPosition.y - _firstTouchPosition.y, _finalTouchPosition.x - _firstTouchPosition.x) * 180 / Mathf.PI;
-        _swipeDistance = Vector2.Distance(_firstTouchPosition, _finalTouchPosition);
-        Debug.Log("angle : " + _swipeAngle + " - " + "distance : " + _swipeDistance);
-    }
-    private void MovePieces(int angle, float distance)
-    {
-        if (distance < 0.15f) return;
-        var row = _backgroundTile.Row;
-        var column = _backgroundTile.Column;
-        if (angle <= 45 && angle > -45 && row < _boad.Width - 1)
-        {
-            // right swipe
-            _boad.SwapDots(_boad.ListBackgroundTile[row, column], _boad.ListBackgroundTile[row + 1, column]);
-        }
-        else if (angle > 45 && angle <= 135 && column < _boad.Height - 1)
-        {
-            // up swipe
-            _boad.SwapDots(_boad.ListBackgroundTile[row, column], _boad.ListBackgroundTile[row, column + 1]);
-        }
-        else if (angle > 135 || angle <= -135 && row > 0)
-        {
-            // left swipe
-            _boad.SwapDots(_boad.ListBackgroundTile[row, column], _boad.ListBackgroundTile[row - 1, column]);
-        }
-        else if (angle < -45 && angle >= -135 && column > 0)
-        {
-            // down swipe
-            _boad.SwapDots(_boad.ListBackgroundTile[row, column], _boad.ListBackgroundTile[row, column-1]);
-        }
-    }
+   
 }
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private Dot _dot;
+    private Direction direction;
+    private Vector2 startPos;
+    private Vector2 endPos; 
+    private Vector2 difference;
+    public float swipeThreshold = 100f;
+    private Board _board;
+
+    private void Awake()
+    {
+        direction = Direction.None;
+        _dot = GetComponent<Dot>();
+        _board = Board.Instance.GetComponent<Board>();
+    }
+
+    private void OnMouseDown()
+    {
+        startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    private void OnMouseUp()
+    {
+        endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Swap();
+    }
+    private Direction SwipegGesture()
+    {
+        difference = endPos - startPos;
+        if( difference.magnitude > swipeThreshold)
+        {
+            if(Mathf.Abs(difference.x) > Mathf.Abs(difference.y))
+            {
+                return direction = difference.x > 0 ? Direction.Right : Direction.Left;
+            }
+            else
+            {
+                return direction = difference.y > 0 ? Direction.Up : Direction.Down;
+            }
+        }else return Direction.None;
+    }
+    private void Swap()
+    {
+        int row = _dot.BackgroundTile.Row;
+        int column = _dot.BackgroundTile.Column;
+        Direction direction = SwipegGesture();
+        switch(direction)
+        {
+            case Direction.Right:
+                if (row < _board.Width - 1) _board.SwapDots(_board.ListBackgroundTile[row, column], _board.ListBackgroundTile[row + 1, column]);
+                break;
+            case Direction.Left:
+                if (row > 0) _board.SwapDots(_board.ListBackgroundTile[row, column], _board.ListBackgroundTile[row - 1, column]);
+                break;
+            case Direction.Up:
+                if (column < _board.Height - 1) _board.SwapDots(_board.ListBackgroundTile[row, column], _board.ListBackgroundTile[row, column + 1]);
+                break;
+            case Direction.Down:
+                if(column > 0) _board.SwapDots(_board.ListBackgroundTile[row, column], _board.ListBackgroundTile[row, column - 1]);
+                break;
+            default:
+                Debug.Log("Dont Swap");
+                break;
+        }
+    }
+    
+
+}
+public enum Direction 
+{ 
+    Left, 
+    Up, 
+    Right, 
+    Down, 
+    None 
+}
