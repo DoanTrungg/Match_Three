4922dddfd1d51080bba1742e562b9bc24c2edf2d
DoanTrungg <150586112+DoanTrungg@users.noreply.github.com>
2024-10-13 21:35:13 +0700

    * match mid column

--- Assets/Scripts/Match.cs
+++ Assets/Scripts/Match.cs
@@ -9,8 +9,8 @@ public class Match : MonoBehaviour
     private Board _board;
     private int _width;
     private int _height;
-    int countMatch;
-    private List<Dot> _listMatched = new List<Dot>();
+    private int countMatch;
+    private HashSet<Dot> _listMatched = new HashSet<Dot>();
     private void Awake()
     {
         //_board = Board.Instance;
@@ -30,17 +30,72 @@ public class Match : MonoBehaviour
 
     public void FindMidMatches(Dot dot)
     {
-        int currentRow = dot.BackgroundTile.Row;
-        int currentColumn = dot.BackgroundTile.Column;
+        bool matchRightRow = MatchRightRow(dot);
+        bool matchLeftRow = MatchLeftRow(dot);
+        bool matchHeadColumn = MatchHeadColumn(dot);
+        bool matchTailColumn = MatchTailColumn(dot);
+
+        MatchMidRow(matchRightRow, matchLeftRow, dot);
+        MatchMidColum(matchHeadColumn, matchTailColumn, dot);
         
-        MatchRightRow(dot, currentRow, currentColumn);
-        MatchLeftRow(dot, currentRow, currentColumn);
-        MatchHeadColumn(dot,currentRow, currentColumn);
-        MatchTailColumn(dot, currentRow, currentColumn);
         Matched(_listMatched);
     }
-    private void MatchRightRow(Dot dot, int currentRow, int currentColumn)
+    private void MatchMidColum(bool matchHead, bool matchTail, Dot dot)
     {
+        int currentRow = dot.BackgroundTile.Row;
+        int currentColumn = dot.BackgroundTile.Column;
+        if (!matchHead && !matchTail)
+        {
+            int countMatchHead = MatchingRowColumn(dot, MatchStatus.HEAD_COLUMN);
+            int countMatchTail = MatchingRowColumn(dot, MatchStatus.TAIL_COLUMN);
+            if (countMatchHead > 0)
+            {
+                for (int columnMatch = 0; columnMatch <= countMatchHead; columnMatch++)
+                {
+                    _listMatched.Add(_board.ListBackgroundTile[currentRow, currentColumn + columnMatch].Dot);
+                }
+                countMatch = 0;
+            }
+            if (countMatchTail > 0)
+            {
+                for (int columnMatch = 0; columnMatch <= countMatchTail; columnMatch++)
+                {
+                    _listMatched.Add(_board.ListBackgroundTile[currentRow, currentColumn - columnMatch].Dot);
+                }
+                countMatch = 0;
+            }
+        }
+    }
+    private void MatchMidRow(bool matchRight,bool matchLeft,Dot dot)
+    {
+        int currentRow = dot.BackgroundTile.Row;
+        int currentColumn = dot.BackgroundTile.Column;
+        if (!matchRight && !matchLeft)
+        {
+            int countMatchRight = MatchingRowColumn(dot, MatchStatus.RIGHT_ROW);
+            int countMatchLeft = MatchingRowColumn(dot, MatchStatus.LEFT_ROW);
+            if (countMatchRight > 0)
+            {
+                for(int rowMatch = 0; rowMatch <= countMatchRight; rowMatch++)
+                {
+                    _listMatched.Add(_board.ListBackgroundTile[currentRow + rowMatch, currentColumn].Dot);
+                }
+                countMatch = 0;
+            }
+            if (countMatchLeft > 0)
+            {
+                for (int rowMatch = 0; rowMatch <= countMatchLeft; rowMatch++)
+                {
+                    _listMatched.Add(_board.ListBackgroundTile[currentRow - rowMatch, currentColumn].Dot);
+                }
+                countMatch = 0;
+            }
+        }
+    }
+    private bool MatchRightRow(Dot dot)
+    {
+        int currentRow = dot.BackgroundTile.Row;
+        int currentColumn = dot.BackgroundTile.Column;
         int minRow = currentRow == 0 ? 1 : -1;
         if (UnMatching(dot, _board.ListBackgroundTile[currentRow + (minRow == 1 ? 0 : -1), currentColumn].Dot, true))
         {
@@ -53,10 +108,14 @@ public class Match : MonoBehaviour
                 }
                 countMatch = 0;
             }
+            return true;
         }
+        else return false;
     }
-    private void MatchLeftRow(Dot dot, int currentRow, int currentColumn)
+    private bool MatchLeftRow(Dot dot)
     {
+        int currentRow = dot.BackgroundTile.Row;
+        int currentColumn = dot.BackgroundTile.Column;
         int maxRow = currentRow == _width - 1 ? 1 : -1;
         if (UnMatching(dot, _board.ListBackgroundTile[currentRow + (maxRow == 1 ? 0 : 1), currentColumn].Dot, true)) // Left_Row
         {
@@ -69,10 +128,13 @@ public class Match : MonoBehaviour
                 }
                 countMatch = 0;
             }
-        }
+            return true;
+        }else return false;
     }
-    private void MatchHeadColumn(Dot dot, int currentRow, int currentColumn)
+    private bool MatchHeadColumn(Dot dot)
     {
+        int currentRow = dot.BackgroundTile.Row;
+        int currentColumn = dot.BackgroundTile.Column;
         int minColumn = currentColumn == 0 ? 1 : -1;
         if (UnMatching(dot, _board.ListBackgroundTile[currentRow, currentColumn + (minColumn == 1 ? 0 : -1)].Dot, false))
         {
@@ -85,10 +147,14 @@ public class Match : MonoBehaviour
                 }
                 countMatch = 0;
             }
+            return true;
         }
+        else return false;
     }
-    private void MatchTailColumn(Dot dot,int currentRow, int currentColumn)
+    private bool MatchTailColumn(Dot dot)
     {
+        int currentRow = dot.BackgroundTile.Row;
+        int currentColumn = dot.BackgroundTile.Column;
         int maxColumn = currentColumn == _height - 1 ? 1 : -1;
         if (UnMatching(dot, _board.ListBackgroundTile[currentRow, currentColumn + (maxColumn == 1 ? 0 : 1)].Dot, false))
         {
@@ -101,14 +167,16 @@ public class Match : MonoBehaviour
                 }
                 countMatch = 0;
             }
+            return true;
         }
+        else return false;
     }
     private bool UnMatching(Dot currentDot, Dot newDot, bool row)
     {
         ID idCurrentDot = currentDot.Id;
         if (row)
         {
-            if (currentDot.Id == newDot.Id) return true;
+            if (currentDot.Id == newDot.Id && currentDot.BackgroundTile.Row == newDot.BackgroundTile.Row) return true;
 
             if ((newDot.BackgroundTile.Row >= 0 || newDot.BackgroundTile.Row < _width) && newDot.Id != idCurrentDot) return true;
 
@@ -117,7 +185,7 @@ public class Match : MonoBehaviour
         else
         {
 
-            if (currentDot.Id == newDot.Id) return true;
+            if (currentDot.Id == newDot.Id && currentDot.BackgroundTile.Column == newDot.BackgroundTile.Column) return true;
 
             if ((newDot.BackgroundTile.Column >= 0 || newDot.BackgroundTile.Column < _height) && newDot.Id != idCurrentDot) return true;
 
@@ -127,70 +195,56 @@ public class Match : MonoBehaviour
     private int MatchingRowColumn(Dot dot, MatchStatus matchStatus)
     {
         ID currentId = dot.Id;
+        int currentRow = dot.BackgroundTile.Row;
+        int currentColumn = dot.BackgroundTile.Column;
+        countMatch = 0;
         switch (matchStatus)
         {
             case MatchStatus.RIGHT_ROW:
                 for (int nextRow = 1; nextRow < 3; nextRow++)
                 {
-                    if (dot.BackgroundTile.Row + nextRow < _width && _board.ListBackgroundTile[dot.BackgroundTile.Row + nextRow, dot.BackgroundTile.Column].Dot.Id == currentId)
+                    if (currentRow + nextRow < _width && _board.ListBackgroundTile[currentRow + nextRow, currentColumn].Dot.Id == currentId)
                     {
                         countMatch++;
                     }
-                    else
-                    {
-                        countMatch = 0;
-                        break;
-                    }
+                    
                 }
                 break;
             case MatchStatus.LEFT_ROW:
                 for (int previousRow = 1; previousRow < 3; previousRow++)
                 {
-                    if (dot.BackgroundTile.Row - previousRow >= 0 && _board.ListBackgroundTile[dot.BackgroundTile.Row - previousRow, dot.BackgroundTile.Column].Dot.Id == currentId)
+                    if (currentRow - previousRow >= 0 && _board.ListBackgroundTile[currentRow - previousRow, currentColumn].Dot.Id == currentId)
                     {
                         countMatch++;
                     }
-                    else
-                    {
-                        countMatch = 0;
-                        break;
-                    }
+                   
                 }
                 break;
             case MatchStatus.HEAD_COLUMN:
                 for(int nextColum = 1; nextColum < 3; nextColum++)
                 {
-                    if(dot.BackgroundTile.Column + nextColum < _height && _board.ListBackgroundTile[dot.BackgroundTile.Row, dot.BackgroundTile.Column + nextColum].Dot.Id == currentId)
+                    if(currentColumn + nextColum < _height && _board.ListBackgroundTile[currentRow, currentColumn + nextColum].Dot.Id == currentId)
                     {
                         countMatch++;
-                    }
-                    else
-                    {
-                        countMatch = 0;
-                        break;
                     }
                 }
                 break;
             case MatchStatus.TAIL_COLUMN:
                 for(int previous = 1; previous < 3; previous++)
                 {
-                    if(dot.BackgroundTile.Column - previous >= 0 && _board.ListBackgroundTile[dot.BackgroundTile.Row, dot.BackgroundTile.Column - previous].Dot.Id == currentId)
+                    if(currentColumn - previous >= 0 && _board.ListBackgroundTile[currentRow, currentColumn - previous].Dot.Id == currentId)
                     {
                         countMatch++;
-                    }
-                    else
-                    {
-                        countMatch = 0;
-                        break;
                     }
                 }
                 break;
             default:
                 break;
         }
+
         return countMatch;
     }
-    private void Matched(List<Dot> listMatched)
+    private void Matched(HashSet<Dot> listMatched)
     {
         if (listMatched.Count < 3 || listMatched == null) return;
         foreach(Dot dot in listMatched)
